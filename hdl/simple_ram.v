24059acc577ad81ba71f72e4ef6301c679a5bac4
Capek System Safety & Robotic Solutions <96583804+MelkorBalrog@users.noreply.github.com>
2025-08-06 22:14:13 -0400

    Add GPL license headers and author info

--- hdl/simple_ram.v
+++ hdl/simple_ram.v
@@ -1,3 +1,20 @@
+// Copyright (C) 2025 Miguel Marina
+// Author: Miguel Marina <karel.capek.robotics@gmail.com>
+// LinkedIn: https://www.linkedin.com/in/progman32/
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 `timescale 1ns/1ps
 // Generic synchronous RAM used for both instruction and data storage.
 module simple_ram #(
